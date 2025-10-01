8b5bb1311afae0c1a14933ec6900e3b8c71ac82d
Harsh Devisha <harshdevisha9111@gmail.com>
2025-09-30 17:27:57 -0700

    adds content for section 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lostcache.github.io",
   base: "/Parallel-Computing",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      rehypeKatex
+    ],
+  },
   integrations: [
     starlight({
       title: "Parallel Computing",
+      customCss: ["./src/custom.css"],
       social: [
         {
           icon: "github",
